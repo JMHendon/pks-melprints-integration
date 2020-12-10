f2b5b4e7f8b59e7be997db5182748cbbfa696693
Jeremy Hendon <hidohebhi@gmail.com>
2020-12-09 22:13:56 -0600

    12/9

--- app.js
+++ app.js
@@ -1,7 +1,168 @@
-const express = require("express");
+const express = require('express');
 const app = express();
 const port = process.env.PORT || 3001;
 
-app.get("/", (req, res) => res.send("Hello from Render!"));
+const axios = require('axios');
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+const all_http_headers = {
+    "Content-type": "application/json",
+    "Accept": "application/json"
+}
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', ['https://maropost-web-app-3.onrender.com', 'http://maropost-web-app-3.onrender.com']);
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Authorization, Accept');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    return next();
+});
+
+app.use(express.json());
+
+
+app.post('/send-order-to-melprints', async function(req,res) {
+    
+    if (req.body.event && req.body.mode) {
+        var pks_event = req.body.event;
+        var pks_mode = req.body.mode;
+    } else {
+        console.log('Either the event or mode field is empty.');
+    }
+
+    let buyer_first_name = (req.body.buyer_first_name !== undefined) ? req.body.buyer_first_name : ''; 
+    let buyer_last_name = (req.body.buyer_last_name !== undefined) ? req.body.buyer_last_name : ''; 
+    let buyer_email = (req.body.buyer_email !== undefined) ? req.body.buyer_email : ''; 
+    let buyer_phone_number = (req.body.buyer_phone !== undefined) ? req.body.buyer_phone : ''; 
+    let shipping_line_1 = (req.body.shpping_address_1 !== undefined) ? req.body.shpping_address_1 : ''; 
+    let shipping_line_2 = (req.body.shpping_address_2 !== undefined) ? req.body.shpping_address_2 : ''; 
+    let shipping_city = (req.body.shipping_city !== undefined) ? req.body.shipping_city : ''; 
+    let shipping_state = (req.body.shipping_state !== undefined) ? req.body.shipping_state : ''; 
+    let shipping_zip = (req.body.shipping_zip !== undefined) ? req.body.shipping_zip : ''; 
+    let shiping_country = (req.body.shiping_country !== undefined) ? req.body.shiping_country : ''; 
+    let ship_method = (req.body.buyer_phone !== undefined) ? req.body.buyer_phone : 'usps_media'; 
+    let product_quantity = (req.body.quantity !== undefined) ? req.body.quantity : ''; 
+
+    if (req.body.product_name product_SKU = (req.body.buyer_phone !== undefined) ? req.body.buyer_phone : ''; 
+
+    
+    let post_data = {
+        'APIKey': process.env.APIKey,
+        'sandbox': 'yes',
+        'Order': {
+            'deliveryContact': {
+                'firstName': buyer_first_name,
+                'lastName': buyer_last_name,
+                'email': buyer_email,
+                'phone': buyer_phone_number,
+                'addressLine1': shipping_line_1,
+                'addressLine2': shipping_line_2,
+                'city': shipping_city,
+                'state': shipping_state,
+                'zip': shipping_zip,
+                'country': shiping_country
+            }
+        }
+        'shipMethod': ship_method,
+        'LineItems': {
+            'productSku': product_SKU,
+            'productQty': product_quantity
+        }
+    }
+    
+    let post_order_to_MelPrints = async (body) => {
+        if (body) {
+          return post_to_MelPrints(body,send_confirmation_email);
+        }
+        return 'Missing Body Data.';
+    }
+
+    if ( pks_event == 'sales' && pks_mode == 'live') {
+        res.send(post_order_to_MelPrints(post_data));
+    } else {
+        console.log('This was not a sales event or it was not live.');
+    }
+
+});
+
+const post_to_MelPrints = (body, callback) => {
+    let melprints_API_URL = process.env.MelPrints_API_URL;
+
+    let axios_Object_Data = {
+        method: 'post',
+        url: melprints_API_URL,
+        headers: all_http_headers,
+        data: body
+    };
+
+    return axios(axios_Object_Data)
+    .then( async response => {
+      const jsonResponse = await response;
+        const data = jsonResponse.data;
+        const response_code = jsonResponse.status;
+        console.log(response_code);
+        console.log(data);
+        console.log("end of log from app.js response");
+        return data;
+    })
+    .catch( error => {
+        return new OperationResult(null, error);
+    });
+
+};
+
+class OperationResult {
+    
+    /**
+     * Constructor
+     * 
+     * @param {object|null} apiResponse
+     * @param {string|null} errorMessage
+     */
+    constructor(apiResponse = null, errorMessage = null) {
+      if (!apiResponse) {
+        this.isSuccess = false;
+        this.errorMessage = errorMessage;
+      }
+      else {
+        const statusCode = apiResponse.status
+        const body = (apiResponse.data) ? apiResponse.data : null
+        this.data = body;
+        this.errorMessage = (this.errorMessage) ? this.errorMessage : (body && body.error) ? body.error : '';
+        if (statusCode >= 200 && statusCode < 300) {
+          this.isSuccess = (this.errorMessage.length == 0);
+        }
+        else {
+          this.isSuccess = false;
+          if (!this.errorMessage) {
+            if (body.message) {
+              this.errorMessage = body.message;
+            }
+            else if (statusCode >= 500) {
+              this.errorMessage = statusCode + ': MelPrints experienced a server error and could not complete your request.';
+            }
+            else if (statusCode >= 400) {
+              this.errorMessage = statusCode + ': Either your authToken or one (or more) of your function arguments are invalid.';
+            }
+            else if (statusCode >= 300) {
+              this.errorMessage = statusCode + ': This MelPrints API function is currently unavailable.';
+            }
+            else {
+              this.errorMessage = statusCode + ': Unexpected final response from MelPrints.';
+            }
+          }
+        }
+      }
+    }
+}
+
+// [function to send email]
+
+// [add 10 minute delay]
+
+// [function to send email on error]
+
+// [Also - need other file to capture shipping confirmations?]
+
+// [capture custom field for phone number - with new conditional]
+
+app.listen(port, () => console.log(`PKS-MelPrints Integration listening on port ${port}!`));
