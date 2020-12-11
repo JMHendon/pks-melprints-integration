db9c9348d0c03ac0519478ea1328a00d9489b015
Jeremy Hendon <hidohebhi@gmail.com>
2020-12-10 20:45:32 -0600

    Update app.js

--- app.js
+++ app.js
@@ -18,6 +18,7 @@ const all_http_headers = {
     "Accept": "application/json"
 }
 
+/** All Headers **/
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', ['https://maropost-web-app-3.onrender.com', 'http://maropost-web-app-3.onrender.com']);
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
@@ -26,8 +27,10 @@ app.use((req, res, next) => {
     return next();
 });
 
+/** PKS USES URL Encoded Format - This app will not read straight JSON **/
 app.use(express.urlencoded({ extended: true }));
 
+/** If we want to send orders directly from PKS to MelPrints - Currently set up only for EKC F+S, but could be set for others. For now, will do this through Maropost then back through separate app below. **/
 app.post('/send-order-to-melprints', async function(req,res) {
 
   let pks_event = req.body.event;
@@ -43,12 +46,13 @@ app.post('/send-order-to-melprints', async function(req,res) {
   let shipping_state = (req.body.shipping_state !== undefined) ? req.body.shipping_state : ''; 
   let shipping_zip = (req.body.shipping_zip !== undefined) ? req.body.shipping_zip : ''; 
   let shipping_country = (req.body.shipping_country !== undefined) ? req.body.shipping_country : ''; 
-  let ship_method = (req.body.custom_shippingMethod !== undefined) ? req.body.custom_shippingMethod : 'usps_media'; 
   let product_quantity = (req.body.quantity !== undefined) ? req.body.quantity : 1;
 
-  console.log(buyer_email);
-  console.log(ship_method);
-  console.log(buyer_phone_number);
+  if (shipping_country === 'US' || !shipping_country) {
+    var ship_method = 'usps_media';
+  } else {
+    var ship_method = 'intl_door_to_door';
+  }
 
   switch (req.body.product_name) {
     case 'Keto Ckbk Shipping (EKC)':
@@ -87,7 +91,8 @@ app.post('/send-order-to-melprints', async function(req,res) {
   console.log(post_data.shipMethod);
   console.log(post_data);
 
-  let confirmation_email_body = {
+  let confirmation_email_data = {
+    'email_recipient': 'hidohebhi@gmail.com',
     'product': product_SKU,
     'subject': 'Order successfully posted to MelPrints',
     'body_of_email': 'The order for ' + buyer_first_name + ' of ' + product_SKU + ' has been posted successfully to Melprints.'
@@ -95,7 +100,7 @@ app.post('/send-order-to-melprints', async function(req,res) {
   
   let post_order_to_MelPrints = async (body) => {
       if (body) {
-        return post_to_MelPrints(body,send_confirmation_email,confirmation_email_body);
+        return post_to_MelPrints(body,send_confirmation_email,confirmation_email_data);
       } else {
         return 'Missing Body Data.';
       }
@@ -153,13 +158,166 @@ const post_to_MelPrints = (body, callback_function,callback_function_body) => {
 
 };
 
-let send_confirmation_email = (confirmation_email_body) => {
+/** App for Putting PKS Order Info (like Address) into Maropost */
+app.post('/pks-order-to-maropost-update-or-create-contact', async function (req, res) {
+
+  let pks_event = req.body.event;
+  let pks_mode = req.body.mode;
+    
+  let buyer_first_name = (req.body.buyer_first_name !== undefined) ? req.body.buyer_first_name : ''; 
+  let buyer_last_name = (req.body.buyer_last_name !== undefined) ? req.body.buyer_last_name : ''; 
+  let buyer_email = (req.body.buyer_email !== undefined) ? req.body.buyer_email : ''; 
+  let buyer_phone_number = (req.body['custom_shipping-phone-number'] !== undefined) ? req.body['custom_shipping-phone-number'] : ''; 
+  let shipping_address1 = (req.body.shipping_address_1 !== undefined) ? req.body.shipping_address_1 : ''; 
+  let shipping_address2 = (req.body.shipping_address_2 !== undefined) ? req.body.shipping_address_2 : ''; 
+  let shipping_city = (req.body.shipping_city !== undefined) ? req.body.shipping_city : ''; 
+  let shipping_state = (req.body.shipping_state !== undefined) ? req.body.shipping_state : ''; 
+  let shipping_zip = (req.body.shipping_zip !== undefined) ? req.body.shipping_zip : ''; 
+  let shipping_country = (req.body.shipping_country !== undefined) ? req.body.shipping_country : '';
+  
+  let billing_address1 = (req.body.billing_address_1 !== undefined) ? req.body.billing_address_1 : ''; 
+  let billing_address2 = (req.body.billing_address_2 !== undefined) ? req.body.billing_address_2 : ''; 
+  let billing_city = (req.body.billing_city !== undefined) ? req.body.billing_city : ''; 
+  let billing_state = (req.body.billing_state !== undefined) ? req.body.billing_state : ''; 
+  let billing_zip = (req.body.billing_zip !== undefined) ? req.body.billing_zip : ''; 
+  let billing_country = (req.body.billing_country !== undefined) ? req.body.billing_country : ''; 
+
+  let contact_info = {
+    'first_name': buyer_first_name,
+    'last_name': buyer_last_name,
+    'email': buyer_email,
+    'custom_field': {
+      'phone': buyer_phone_number,
+      'shipping_address1': shipping_address1,
+      'shipping_address2': shipping_address2,
+      'shipping_city': shipping_city,
+      'shipping_state': shipping_state,
+      'shipping_zip': shipping_zip,
+      'shipping_country': shipping_country,
+      'billing_address1': billing_address1,
+      'billing_address2': billing_address2,
+      'billing_city': billing_city,
+      'billing_state': billing_state,
+      'billing_zip': billing_zip,
+      'billing_country': billing_country
+    }
+  };
+  
+  res.send(add_or_update_contact_in_maropost(buyer_email,contact_info));
+
+})
+
+/** General Function to Get Contact Info By Email **/
+
+const get_contact_info = (email_address) => {
+  let mp_get_contact_info_url = 'https://api.maropost.com/accounts/2264/contacts/email.json?contact[email]=' + email_address;
+
+  let axios_get_contact_input_data = {
+    method: 'get',
+    url: mp_get_contact_info_url,
+    headers: all_http_headers
+  };
+
+  return axios(axios_get_contact_input_data)
+  .then( async response => {
+    const jsonResponse = await response;
+    console.log(jsonResponse);
+    console.log(jsonResponse.data);
+    console.log(jsonResponse.data.uid);
+    return jsonResponse;
+  })
+  .catch( error => {
+      return new OperationResult(null, error);
+  });
+
+}
+
+/** General Function to Add Contact to Maropost (without specifying list) **/
+
+const add_contact_to_maropost = (contact_data_object) => {
+    
+  let mp_add_contact_url = 'https://api.maropost.com/accounts/2264/contacts.json';
+
+  let axios_Object_Data = {
+      method: 'post',
+      url: mp_add_contact_url,
+      headers: all_http_headers,
+      data: contact_data_object
+  };
+
+  return axios(axios_Object_Data)
+  .then( async response => {
+    const jsonResponse = await response;
+      console.log(jsonResponse);
+      console.log(jsonResponse.data);
+      return jsonResponse;
+  })
+  .catch( error => {
+      return new OperationResult(null, error);
+  });
+
+}
+
+/** General Function to Update Contact in Maropost (without specifying list) **/
+
+const update_contact_in_maropost = (uid,contact_data_object) => {
+    
+  let mp_update_contact_url = 'https://api.maropost.com/accounts/2264/contacts/' + uid + '.json';
+
+  let axios_Object_Data = {
+      method: 'put',
+      url: mp_update_contact_url,
+      headers: all_http_headers,
+      data: contact_data_object
+  };
+
+  return axios(axios_Object_Data)
+  .then( async response => {
+    const jsonResponse = await response;
+      console.log(jsonResponse);
+      console.log(jsonResponse.data);
+      return jsonResponse;
+  })
+  .catch( error => {
+      return new OperationResult(null, error);
+  });
+
+}
+
+
+/** Function to Add or Update Contact in Maropost - Depending on if Exists **/
+
+const add_or_update_contact_in_maropost = (email_address, contact_data_object) => {
+  
+  const delayed_get_and_update_function = () => {
+    let contact_info_object = await get_contact_UID(email_address);
+    let contact_UID = contact_info_object.data.uid;
+
+    if (contact_UID) {
+      return update_contact_in_maropost(contact_UID,contact_data_object);    
+    } else {
+      return add_contact_to_maropost(contact_data_object);
+    }
+  }
+  
+  return setTimeout(delayed_get_and_update_function,150000);
+
+}
+
+
+
+
+
+
+
+/** Sends emails from Jeremy@Nourishing.group **/
+let send_confirmation_email = (email_data) => {
 
   var mailOptions = {
     from: process.env.gmail_user_name,
-    to: 'hidohebhi@gmail.com',
-    subject: confirmation_email_body.subject,
-    text: confirmation_email_body.body_of_email
+    to: email_data.email_recipient,
+    subject: email_data.subject,
+    text: email_data.body_of_email
   };
   
   transporter.sendMail(mailOptions, function(error, info){
@@ -219,16 +377,10 @@ class OperationResult {
 
 
 
-// [add 45 minute delay]
-
 // [function to send email on error]
 
-// [Also - need other file to capture shipping confirmations?]
+// [Also - need other file to capture shipping confirmations - or could do it here]
 
-// [capture custom field for phone number - with new conditional]
-
-// [create full array for product_name]
-
-// [for app.post, send back to somewhere else other than PKS? Send confirmation email?]
+// [create full list for product_name - of other products]
 
 app.listen(port, () => console.log(`PKS-MelPrints Integration listening on port ${port}!`));
